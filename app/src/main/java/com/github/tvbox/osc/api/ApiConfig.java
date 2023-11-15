241f7d7db48b93f9d8d749ba2b1ccf0d0898157c
slasjh <127065284+slasjh@users.noreply.github.com>
2023-11-15 15:26:11 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/mfkdy/cy/-/raw/main/local.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
